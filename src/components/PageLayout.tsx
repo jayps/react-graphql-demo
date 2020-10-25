02c4c2b10db016a5713af9a6dba4a5b89f7eeef9
= <=>
2020-10-25 17:25:09 +0200

    Fixed layout issue

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Col, Container, Row, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Nav, NavbarText} from "reactstrap";
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import {AuthorsContext} from "../contexts/authors/AuthorsContext";
 
 export type PageLayoutProps = {
@@ -19,21 +19,17 @@ const PageLayout: React.FC<PageLayoutProps> = ({children}: PageLayoutProps) => {
 
     return (
         <React.Fragment>
-            <Row>
-                <Col>
-                    <Navbar color="light" light expand="md">
-                        <NavbarBrand href="/">Book store demo</NavbarBrand>
-                        <NavbarToggler onClick={toggle} />
-                        <Collapse isOpen={isOpen} navbar>
-                            <Nav className="mr-auto" navbar>
-                                <NavItem>
-                                    <NavLink to="/authors/">Authors</NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </Navbar>
-                </Col>
-            </Row>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">Book store demo</NavbarBrand>
+                <NavbarToggler onClick={toggle}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                            <NavLink to="/authors/">Authors</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
             <Container>
                 {children}
             </Container>
